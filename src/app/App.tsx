58a6390d6638a641ace0565126f4a70a4dbbefb2
kunukn <3199603+kunukn@users.noreply.github.com>
2026-05-24 15:20:37 +0200

    update: responsive fiddling

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div
       id="app"
-      className="flex flex-1 flex-col items-center md:justify-center-safe"
+      className="flex flex-1 flex-col items-center justify-center-safe"
     >
       <RouterProvider router={router} />
     </div>
